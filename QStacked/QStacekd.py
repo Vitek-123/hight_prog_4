87231da8d0b7175d0c676e9c97199c085f68abed
Victor <Vctorsenin@yandex.ru>
2026-01-18 14:18:10 +0300

    99%

--- QStacked/QStacekd.py
+++ QStacked/QStacekd.py
@@ -104,6 +104,16 @@ class Main(QMainWindow):
         frame_hight_right_tab_1.setLineWidth(1)
         frame_hight_right_tab_1.setFrameShape(QFrame.Shape.Box)
 
+        vertical_hight_right_tab_1 = QVBoxLayout(frame_hight_right_tab_1)
+        vertical_2_hight_right_tab_1 = QVBoxLayout()
+
+        vertical_hight_right_tab_1.addWidget(QLabel("Ресурсы"))
+        vertical_2_hight_right_tab_1.addWidget(QLabel("Золото: 12500"))
+        vertical_2_hight_right_tab_1.addWidget(QLabel("Кристалы: 450"))
+        vertical_2_hight_right_tab_1.addWidget(QLabel("Энергия 85/100"))
+
+        vertical_hight_right_tab_1.addLayout(vertical_2_hight_right_tab_1)
+
         splitter_tab_1.addWidget(frame_hight_left_tab_1)
         splitter_tab_1.addWidget(frame_hight_right_tab_1)
 
@@ -113,13 +123,33 @@ class Main(QMainWindow):
 
         splitter_tab_2 = QSplitter(QtCore.Qt.Orientation.Horizontal)
 
+        # splitter 2 left
+
         frame_bottom_left_tab_1 = QFrame()
         frame_bottom_left_tab_1.setFrameShape(QFrame.Shape.Box)
         frame_bottom_left_tab_1.setLineWidth(1)
 
+        vertical_bottom_left_tab_1 = QVBoxLayout(frame_bottom_left_tab_1)
+        vertical_2_bottom_left_tab_1 = QVBoxLayout()
+        vertical_bottom_left_tab_1.addWidget(QLabel("Инвентарь"))
+        vertical_bottom_left_tab_1.addLayout(vertical_2_bottom_left_tab_1)
+        vertical_2_bottom_left_tab_1.addWidget(QLabel("Оружие: 8"))
+        vertical_2_bottom_left_tab_1.addWidget(QLabel("Броня: 5"))
+        vertical_2_bottom_left_tab_1.addWidget(QLabel("Зелья: 12"))
+
+        # splitter 2 right
+
         frame_bottom_right_tab_1 = QFrame()
         frame_bottom_right_tab_1.setFrameShape(QFrame.Shape.Box)
         frame_bottom_right_tab_1.setLineWidth(1)
+
+        vertical_bottom_right_tab_1 = QVBoxLayout(frame_bottom_right_tab_1)
+        vertical_2_bottom_right_tab_1 = QVBoxLayout()
+        vertical_bottom_right_tab_1.addWidget(QLabel("Навыки"))
+        vertical_bottom_right_tab_1.addLayout(vertical_2_bottom_right_tab_1)
+        vertical_2_bottom_right_tab_1.addWidget(QLabel("Атака: 75"))
+        vertical_2_bottom_right_tab_1.addWidget(QLabel("Защита: 60"))
+        vertical_2_bottom_right_tab_1.addWidget(QLabel("Магия: 45"))
 
         splitter_tab_2.addWidget(frame_bottom_left_tab_1)
         splitter_tab_2.addWidget(frame_bottom_right_tab_1)
@@ -129,6 +159,57 @@ class Main(QMainWindow):
         # tab 2
 
         tab2 = QWidget()
+
+        vertical_tab_2 = QVBoxLayout(tab2)
+
+        # tab 2 hight
+
+        frame_hight_tab_2 = QFrame()
+        frame_hight_tab_2.setFrameShape(QFrame.Shape.Box)
+        frame_hight_tab_2.setLineWidth(1)
+
+        vertical_tab_2_hight = QVBoxLayout(frame_hight_tab_2)
+
+        vertical_tab_2_hight.addWidget(QLabel("Квесты"))
+
+        vertical_tab_2_hight_2 = QVBoxLayout()
+        vertical_tab_2_hight_2.addWidget(QLabel("Активныйе: 3"))
+        vertical_tab_2_hight_2.addWidget(QLabel("Выполненые: 15"))
+        vertical_tab_2_hight_2.addWidget(QLabel("Доступные: 5"))
+
+        vertical_tab_2_hight.addLayout(vertical_tab_2_hight_2)
+
+        # tab 2 middle
+
+        frame_middle_tab_2 = QFrame()
+        frame_middle_tab_2.setFrameShape(QFrame.Shape.Box)
+        frame_middle_tab_2.setLineWidth(1)
+
+        vertical_tab_2_middle = QVBoxLayout(frame_middle_tab_2)
+        vertical_tab_2_middle.addWidget(QLabel("Локации"))
+
+        vertical_tab_2_middle_2 = QVBoxLayout()
+        vertical_tab_2_middle.addLayout(vertical_tab_2_middle_2)
+        vertical_tab_2_middle_2.addWidget(QLabel("Открыто 8/12"))
+        vertical_tab_2_middle_2.addWidget(QLabel("Исследовано: 65%"))
+
+        # tab 2 bottom
+
+        frame_bottom_tab_2 = QFrame()
+        frame_bottom_tab_2.setFrameShape(QFrame.Shape.Box)
+        frame_bottom_tab_2.setLineWidth(1)
+
+        vertical_tab_2_bottom = QVBoxLayout(frame_bottom_tab_2)
+        vertical_tab_2_bottom.addWidget(QLabel("Прогресс навыков"))
+        vertical_tab_2_bottom_2 = QVBoxLayout()
+        vertical_tab_2_bottom.addLayout(vertical_tab_2_bottom_2)
+        vertical_tab_2_bottom_2.addWidget(QLabel("Боевые: 75%"))
+        vertical_tab_2_bottom_2.addWidget(QLabel("Магические: 40%"))
+        vertical_tab_2_bottom_2.addWidget(QLabel("Ремесло: 55%"))
+
+        vertical_tab_2.addWidget(frame_hight_tab_2)
+        vertical_tab_2.addWidget(frame_middle_tab_2)
+        vertical_tab_2.addWidget(frame_bottom_tab_2)
 
         table.addTab(tab1, "Характеристики")
         table.addTab(tab2, "Прогресс")
