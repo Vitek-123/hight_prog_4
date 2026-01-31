e0da7049bf8df22b5eecdeaa08ff57f8af329426
Victor <Vctorsenin@yandex.ru>
2026-02-01 01:11:43 +0300

    create content

--- bd_env/main.py
+++ bd_env/main.py
@@ -44,6 +44,8 @@ class Main(QMainWindow):
         self.central_button_next.clicked.connect(self.go_next)
         self.central_button_back.clicked.connect(self.go_back)
 
+        #bd
+
         self.technic = self.db.technic()
         self.count_product = len(self.technic)
         self.count_page = math.ceil(self.count_product/5)
@@ -66,12 +68,36 @@ class Main(QMainWindow):
             self.scrollarea.setWidget(self.content_scroll_widget)
             self.stacked.addWidget(self.content_widget)
             self.stop_product = 0
-            i = self.stop_product
+
             for i in self.technic:
-                if i[0] % 6 == 0:
-                    break
+                self.content_scroll_vertical_horizontal = QHBoxLayout()
+                self.content_scroll_vertical.addLayout(self.content_scroll_vertical_horizontal)
+
+                self.content_scroll_pictures = QLabel()
+                self.content_scroll_pictures.setPixmap(QtGui.QPixmap(f"resourse/icon{i[0]}.png").scaled(100, 100))
+
+                self.content_scroll_info = QLabel("Подробнее")
+                self.content_scroll_info.setFrameShape(QFrame.Shape.Box)
+
+                self.content_scroll_info.mousePressEvent = lambda event: self.more_info(db)
+
+                self.content_scroll_sale = QLabel()
+                self.content_scroll_sale.setFrameShape(QFrame.Shape.Box)
+                if i[2] == "Lenovo":
+                    self.content_scroll_sale.setText("Скидка - 15%")
                 else:
-                    self.content_scroll_vertical.addWidget(QLabel(f"{i[0]}"))
+                    self.content_scroll_sale.setText("Скидка - 0%")
+
+
+
+                self.content_scroll_vertical_horizontal.addWidget(self.content_scroll_pictures)
+                self.content_scroll_vertical_horizontal.addWidget(self.content_scroll_info, stretch=5)
+                self.content_scroll_vertical_horizontal.addWidget(self.content_scroll_sale, stretch=1)
+                if (i[0]) % 5 == 0:
+                    self.technic = self.technic[5:]
+                    break
+
+
 
 
         # Добавление
@@ -85,6 +111,18 @@ class Main(QMainWindow):
     def go_back(self):
         self.stacked.setCurrentIndex(self.stacked.currentIndex()-1)
 
+    def show_more_info(self):
+        self.more_info = MoreInfo(self.db)
+        self.more_info.show()
+
+class MoreInfo(QWidget):
+    def __init__(self, db):
+        self.db = db
+        super().__init__()
+        self.resize(200, 150)
+        self.setWindowTitle("Подробная информация")
+
+        print(self.db.technic())
 
 if __name__ == "__main__":
     import sys
